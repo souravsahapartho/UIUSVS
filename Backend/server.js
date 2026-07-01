efc0663e12a2d9da6d3afb0ccb2b189db1d81403
Sourav Saha <souravsahapartho@gmail.com>
2026-07-01 15:55:02 +0600

    Backend/server.js

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,13 @@ const pool = mysql.createPool({
   user: process.env.TIDB_USER,
   password: process.env.TIDB_PASSWORD,
   database: process.env.TIDB_DATABASE,
-  ssl: { minVersion: "TLSv1.2", rejectUnauthorized: true },
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 // ---- Cloudinary Config ----
